57500b5038728b08c03153834f864de66d2ab1d5
davidshin0302 <david.shin0302@gmail.com>
2024-10-16 23:58:19 -0400

    Initial commit and pushing existing repository to new repo.

---
+++ src/main/java/com/safetynet/alerts/SafetyNetAlertsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsBackendApplication.class, args);
+	}
+
+}
