a71ec4ea0695b2cc56e6abe9a85a03c9480e056e
github username <github email address>
2024-10-31 12:27:29 -0400

    sprint-2: converted @controller to @RestController and implement simple GetMapping logic for PersonController.

--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -1,7 +1,25 @@
 package com.safetynet.alerts.controller;
 
-import org.springframework.stereotype.Controller;
+import com.safetynet.alerts.model.Person;
+import com.safetynet.alerts.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class PersonController {
+
+    @Autowired
+    PersonService personService;
+
+    @GetMapping("/person")
+    public List<Person> getAllPersonList() {
+        return personService.getAllPersonList();
+    }
+
+    
 }
