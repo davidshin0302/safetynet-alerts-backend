1f8a48e5d888f7dbf52373df282511a2d52725d8
github username <github email address>
2024-10-31 12:26:26 -0400

    sprint-2: converted CRUDRepository to JpaRepository

--- src/main/java/com/safetynet/alerts/repository/MedicalRecordRepository.java
+++ src/main/java/com/safetynet/alerts/repository/MedicalRecordRepository.java
@@ -1,9 +1,9 @@
 package com.safetynet.alerts.repository;
 
 import com.safetynet.alerts.model.MedicalRecord;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MedicalRecordRepository extends CrudRepository<MedicalRecord, Long> {
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
 }
