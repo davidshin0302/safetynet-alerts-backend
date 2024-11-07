1c2d6c9de6e6b0ef539a7a0ad6945d5c2d6a7dfe
github username <github email address>
2024-11-07 13:14:33 -0500

    sprint-2: removed @NotBlank annotation except firstName and lastName. override equals, and hashcode method.

--- src/main/java/com/safetynet/alerts/model/Person.java
+++ src/main/java/com/safetynet/alerts/model/Person.java
@@ -12,26 +12,19 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Person {
 
-    private Long id;
     @NotBlank
     private String firstName;
     @NotBlank
     private String lastName;
-    @NotBlank
     private String address;
-    @NotBlank
     private String city;
-    @NotBlank
     private String zip;
-    @NotBlank
     private String phone;
-    @NotBlank
     private String email;
 
     @Override
     public String toString() {
         return "Person{" +
-                "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", address='" + address + '\'' +
@@ -40,5 +33,20 @@ public class Person {
                 ", phone='" + phone + '\'' +
                 ", email='" + email + '\'' +
                 '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+
+        return firstName.equalsIgnoreCase(person.firstName) && lastName.equalsIgnoreCase(person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
     }
 }
