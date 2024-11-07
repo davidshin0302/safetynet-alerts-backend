73a1016a845d8aefde5c910f525906007d9c7460
davidshin0302 <david.shin0302@gmail.com>
2024-11-07 01:26:49 -0500

    sprint2: renamed folder

--- src/main/java/com/safetynet/alerts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/safetynet/alerts/exception/GlobalExceptionHandler.java
@@ -1,11 +1,10 @@
-package com.safetynet.alerts.Exception;
+package com.safetynet.alerts.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
