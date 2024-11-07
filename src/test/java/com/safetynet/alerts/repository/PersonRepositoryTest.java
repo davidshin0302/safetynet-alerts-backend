dcf0d0f3b1086e1e14d4bd6c2b9901592f406a87
github username <github email address>
2024-11-07 16:33:10 -0500

    sprint-2: implementing in memory use of data json file.

--- src/test/java/com/safetynet/alerts/repository/PersonRepositoryTest.java
+++ src/test/java/com/safetynet/alerts/repository/PersonRepositoryTest.java
@@ -2,12 +2,19 @@ package com.safetynet.alerts.repository;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.alerts.model.DataObject;
 import com.safetynet.alerts.model.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,11 +22,28 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {PersonRepository.class})
 public class PersonRepositoryTest {
-    @Autowired
-    private PersonRepository personRepository;
+
+    PersonRepository personRepository;
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        objectMapper = new ObjectMapper();
+
+        String filePath = "/data.json";
+        /*Read the Json file into Memory: Load the data from data.json into a list of Person objects.
+        Perform the deletion in Memory: Create a new list that excludes the target Person without modifiying the original list.
+        get.Class().getResourceAsStream(): Looks for resources in the classpath, typically under the src/main/resources
+         */
+        InputStream inputStream = getClass().getResourceAsStream(filePath);
+        List<Person> personList = objectMapper.readValue(inputStream, DataObject.class).getPersons();
+
+        List<Person> testPersonList = new ArrayList<>(personList);
+        personRepository = new PersonRepository(testPersonList);
+    }
 
     @Test
-    public void testFindALlBySize() {
+    public void testFindAllBySize() {
         //GIVEN
         var expected = 23;
         //WHEN
@@ -29,8 +53,7 @@ public class PersonRepositoryTest {
     }
 
     @Test
-    public void testFndByFirstAndLastName() throws JsonProcessingException {
-        var objectMapper = new ObjectMapper();
+    public void testFindByFirstAndLastName() throws JsonProcessingException {
         var expectedJson = "{ \"firstName\":\"John\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6512\", \"email\":\"jaboyd@email.com\" }";
 
         var expectedPerson = objectMapper.readValue(expectedJson, Person.class);
@@ -43,5 +66,18 @@ public class PersonRepositoryTest {
         assertEquals(expectedPerson.getCity(), actual.getCity());
         assertEquals(expectedPerson.getZip(), actual.getZip());
         assertEquals(expectedPerson.getEmail(), actual.getEmail());
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        var expectedJson = "{ \"firstName\":\"John\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6512\", \"email\":\"jaboyd@email.com\" }";
+        var expectedPerson = objectMapper.readValue(expectedJson, Person.class);
+        var expectedSizePersonList = 22;
+
+        personRepository.delete(expectedPerson);
+
+        var actualSizePersonList = personRepository.findAll().size();
+
+        assertEquals(expectedSizePersonList, actualSizePersonList);
     }
 }
