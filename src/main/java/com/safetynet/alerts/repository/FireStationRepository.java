198d6ee44fc8df241922ce44c39be97a5aa056c0
davidshin0302 <david.shin0302@gmail.com>
2024-11-07 01:26:38 -0500

    sprint2: refactored repository files.

--- src/main/java/com/safetynet/alerts/repository/FireStationRepository.java
+++ src/main/java/com/safetynet/alerts/repository/FireStationRepository.java
@@ -1,9 +1,9 @@
 package com.safetynet.alerts.repository;
 
-import com.safetynet.alerts.model.FireStation;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FireStationRepository extends JpaRepository<FireStation, Long> {
+@Slf4j
+public class FireStationRepository {
 }
--- src/main/java/com/safetynet/alerts/repository/PersonRepository.java
+++ src/main/java/com/safetynet/alerts/repository/PersonRepository.java
@@ -1,9 +1,67 @@
 package com.safetynet.alerts.repository;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetynet.alerts.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Repository;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//TODO: Read and parse data json File into java instant class.
+// Each class should have contructors, default etc...
+
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
+@Slf4j
+public class PersonRepository {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final String filePath = "src/main/resources/data.json";
+
+
+    public List<Person> findAll() {
+        try {
+            return objectMapper.readValue(new File(filePath), new TypeReference<List<Person>>() {
+            });
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
+        }
+
+        return List.of();
+    }
+
+    public Person findById(Long id) {
+        return findAll().stream().filter(person -> person.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        List<Person> people = new ArrayList<Person>();
+
+        findAll().forEach(person -> {
+            if (!person.getId().equals(id)) {
+                people.add(person);
+            }
+        });
+
+        try {
+            objectMapper.writeValue(new File(filePath), people);
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
+        }
+    }
+
+    public void save(Person person) {
+        List<Person> people = findAll();
+
+        try {
+            people.add(person);
+            objectMapper.writeValue(new File(filePath), people);
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
+        }
+    }
 }
