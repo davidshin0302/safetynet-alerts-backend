cb15db48e1659dbe9d8cac1c95d39617852d5c92
github username <github email address>
2024-11-12 14:58:38 -0500

    sprint-2: setup FireStation Controller class for Http Request handler.

--- src/main/java/com/safetynet/alerts/controller/FireStationController.java
+++ src/main/java/com/safetynet/alerts/controller/FireStationController.java
@@ -1,7 +1,73 @@
 package com.safetynet.alerts.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.alerts.model.FireStation;
+import com.safetynet.alerts.repository.FireStationRepository;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
+@RequestMapping("/firestation")
+@Slf4j
 public class FireStationController {
+    @Autowired
+    private FireStationRepository fireStationRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @GetMapping
+    public ResponseEntity<String> getPeople() {
+
+        try {
+            String fireStationList = objectMapper.writeValueAsString(fireStationRepository.findAll());
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(fireStationList);
+        } catch (JsonProcessingException ex) {
+            log.error("Error at serializing data: {}", ex.getMessage());
+            return new ResponseEntity<>("[FireStationController]: ", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<FireStation> addFireStation(@Valid @RequestBody FireStation fireStation) {
+        if (fireStationRepository.save(fireStation)) {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(fireStationRepository.findByAddress(fireStation));
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<FireStation> updateExistingFireStation(@RequestBody FireStation fireStation) throws IOException {
+        if (fireStationRepository.updateExistingFireStation(fireStation)) {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(fireStationRepository.findByAddress(fireStation));
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deleteExistingFireStation(@RequestBody FireStation fireStation) {
+        if (fireStationRepository.delete(fireStation)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
