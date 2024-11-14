77f7d1fa29a75cacd16053f100429f79f29a17d2
github username <github email address>
2024-11-14 16:31:50 -0500

    sprint-2: Set up basic structure.

--- src/test/java/com/safetynet/alerts/controller/FireStationControllerTest.java
+++ src/test/java/com/safetynet/alerts/controller/FireStationControllerTest.java
@@ -5,23 +5,29 @@ import com.safetynet.alerts.model.DataObject;
 import com.safetynet.alerts.model.FireStation;
 import com.safetynet.alerts.repository.FireStationRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@Disabled
 @WebMvcTest(FireStationController.class)
 class FireStationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private FireStationRepository  fireStationRepository;
+    private FireStationRepository  fireStationRepository; // need to train the mock bean.
 
     private String fireStationJsonList;
     private String newFireStationFile;
@@ -32,11 +38,16 @@ class FireStationControllerTest {
     @BeforeEach
     void setUp() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        List<FireStation> fireStationList = objectMapper.readValue(new File(TEST_FILE_PATH), DataObject.class).getFireStations();
+        List<FireStation> fireStationList = objectMapper.readValue(new File(TEST_FILE_PATH + "/testData.json"), DataObject.class).getFireStations();
     }
 
     @Test
-    void getPeople() {
+    void testGetFireStation() throws Exception {
+        mockMvc.perform(get("/firestation")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        //aseert = number of fireStation size
     }
 
     @Test
