1dbb55ace47a45ab71d191df6589c436b267c6f9
github username <github email address>
2024-11-19 14:57:04 -0500

    sprint-2: implemented getAllMedicalRecords() test case.

--- src/test/java/com/safetynet/alerts/controller/MedicalRecordControllerTest.java
+++ src/test/java/com/safetynet/alerts/controller/MedicalRecordControllerTest.java
@@ -1,13 +1,46 @@
 package com.safetynet.alerts.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.alerts.model.DataObject;
+import com.safetynet.alerts.repository.MedicalRecordRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(MedicalRecordController.class)
 class MedicalRecordControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MedicalRecordRepository medicalRecordRepository;
+
+    private MedicalRecordController medicalRecordController;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String TEST_FILE_PATH = "src/test/resources";
+
     @Test
-    void getAllMedicalRecords() {
+    void getAllMedicalRecords() throws Exception {
+        when(medicalRecordRepository.findAll()).thenReturn(objectMapper.readValue(new File(TEST_FILE_PATH + "/testData.json"), DataObject.class).getMedicalRecords());
+
+        mockMvc.perform(get("/medicalrecords").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(23)));
+
     }
 
     @Test
