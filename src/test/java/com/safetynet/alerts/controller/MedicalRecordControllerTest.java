aad0e26d8221fe651923d81bae3c8922a12949e9
davidshin0302 <david.shin0302@gmail.com>
2024-11-20 01:33:55 -0500

    sprint2: implemented update and delete handler test case.

--- src/test/java/com/safetynet/alerts/controller/MedicalRecordControllerTest.java
+++ src/test/java/com/safetynet/alerts/controller/MedicalRecordControllerTest.java
@@ -2,6 +2,7 @@ package com.safetynet.alerts.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetynet.alerts.model.DataObject;
+import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.repository.MedicalRecordRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.io.File;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MedicalRecordController.class)
@@ -28,7 +31,7 @@ class MedicalRecordControllerTest {
 
     private MedicalRecordController medicalRecordController;
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     private static final String TEST_FILE_PATH = "src/test/resources";
 
@@ -44,6 +47,22 @@ class MedicalRecordControllerTest {
     }
 
     @Test
-    void addMedicalRecords() {
+    void addMedicalRecord() throws Exception {
+        MedicalRecord medicalRecord = objectMapper.readValue(new File(TEST_FILE_PATH + "/medicalRecordDir/testNewMedicalRecord.json"), MedicalRecord.class);
+        when(medicalRecordRepository.save(any(MedicalRecord.class))).thenReturn(true);
+
+        mockMvc.perform(post("/medicalRecord").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(medicalRecord)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void addMedicalRecord_conflict() throws Exception {
+        MedicalRecord medicalRecord = new MedicalRecord();
+        when(medicalRecordRepository.save(any(MedicalRecord.class))).thenReturn(false);
+
+        mockMvc.perform(post("/medicalRecord").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(medicalRecord)))
+                .andExpect(status().isConflict());
     }
 }
