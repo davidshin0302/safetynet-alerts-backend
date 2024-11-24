7942062a1e6747d41dcd6756291d38189077f670
davidshin0302 <david.shin0302@gmail.com>
2024-11-23 19:44:42 -0500

    sprint2: added @NoArgsConstructor

--- src/main/java/com/safetynet/alerts/view/PersonInfoView.java
+++ src/main/java/com/safetynet/alerts/view/PersonInfoView.java
@@ -1,21 +1,16 @@
 package com.safetynet.alerts.view;
 
-import com.safetynet.alerts.model.MedicalRecord;
-import com.safetynet.alerts.model.Person;
-import com.safetynet.alerts.repository.MedicalRecordRepository;
-import com.safetynet.alerts.repository.PersonRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDate;
 import java.util.List;
 
-//Might need to
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PersonInfoView {
     private String name;
     private String address;
