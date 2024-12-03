2de6d37a15456d03bfaf67f1f04d0fc21149aabd
github username <github email address>
2024-12-03 17:37:00 -0500

    sprint-3: deleted unused code.

--- src/main/java/com/safetynet/alerts/model/FireStation.java
+++ src/main/java/com/safetynet/alerts/model/FireStation.java
@@ -13,19 +13,4 @@ public class FireStation {
 
     private String address;
     private String station;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof FireStation fireStation)) return false;
-
-        return address.equals(fireStation.getAddress()) && station.equals(fireStation.getStation());
-    }
-
-    @Override
-    public int hashCode() {
-        int result = address.hashCode();
-        result = 31 * result + station.hashCode();
-        return result;
-    }
 }
