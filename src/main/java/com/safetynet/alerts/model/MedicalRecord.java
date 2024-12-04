2abb51cabd567d4af2a3cb1b16aa8aa0dfc6b7b4
github username <github email address>
2024-12-04 10:56:55 -0500

    sprint-3: bringing back delete files.

--- src/main/java/com/safetynet/alerts/model/MedicalRecord.java
+++ src/main/java/com/safetynet/alerts/model/MedicalRecord.java
@@ -34,6 +34,16 @@ public class MedicalRecord {
                 '}';
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MedicalRecord medicalRecord)) return false;
+
+        return firstName.equalsIgnoreCase(medicalRecord.getFirstName())
+                && lastName.equalsIgnoreCase(medicalRecord.getLastName())
+                && birthdate.equalsIgnoreCase(medicalRecord.getBirthdate());
+    }
+
     @JsonIgnore
     public String getUniqueIdentifier() {
         return firstName + "_" + lastName;
--- src/main/java/com/safetynet/alerts/model/Person.java
+++ src/main/java/com/safetynet/alerts/model/Person.java
@@ -23,6 +23,36 @@ public class Person {
     private String phone;
     private String email;
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+
+        return firstName.equalsIgnoreCase(person.getFirstName())
+                && lastName.equalsIgnoreCase(person.getLastName())
+                && email.equalsIgnoreCase(person.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
+    }
+
     @JsonIgnore
     public String getUniqueIdentifier() {
         return firstName + "_" + lastName;
