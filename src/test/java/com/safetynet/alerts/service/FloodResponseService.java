13f162e99a9c82de4da90eafc588b706e3cefd7b
github username <github email address>
2024-12-04 12:51:35 -0500

    sprint-3: working to implement floodresponseService class.

---
+++ src/test/java/com/safetynet/alerts/service/FloodResponseService.java
@@ -0,0 +1,37 @@
+package com.safetynet.alerts.service;
+
+public class FloodResponseService {
+
+    /*
+    TODO:: Implement flood station response service based on this json object.
+    {
+  "stations": [
+    {
+      "stationNumber": 1,
+      "households": [
+        {
+          "address": "123 Main St",
+          "residents": [
+            {
+              "name": "John Doe",
+              "phone": "555-1234",
+              "age": 34,
+              "medications": ["ibuprofen:200mg"],
+              "allergies": ["peanuts"]
+            },
+            {
+              "name": "Jane Doe",
+              "phone": "555-1234",
+              "age": 30,
+              "medications": ["acetaminophen:500mg"],
+              "allergies": []
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+     */
+}
