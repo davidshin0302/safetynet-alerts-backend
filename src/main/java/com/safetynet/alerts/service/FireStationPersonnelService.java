c18e83405b5a87f0dba98c8652b34e98181a0090
davidshin0302 <david.shin0302@gmail.com>
2024-12-13 16:47:22 -0500

    sprint3: working on the logic

--- src/main/java/com/safetynet/alerts/service/FireStationPersonnelService.java
+++ src/main/java/com/safetynet/alerts/service/FireStationPersonnelService.java
@@ -1,15 +1,61 @@
 package com.safetynet.alerts.service;
 
+import com.safetynet.alerts.view.FireResponse;
 import com.safetynet.alerts.view.FireStationPersonnel;
+import com.safetynet.alerts.view.OtherPersonInfo;
+import com.safetynet.alerts.view.Resident;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class FireStationPersonnelService {
+    @Autowired
+    private FireResponseService fireResponseService;
 
-    public FireStationPersonnel findFireStationPersonnel(String stationNumber){
+    public FireStationPersonnel findFireStationPersonnel(String stationNumber) {
+        int adultCount;
+        int childCount;
+        Map<String, FireResponse> fireResponseMap = fireResponseService.getFireResponse();
+        FireStationPersonnel fireStationPersonnel = new FireStationPersonnel();
+        List<OtherPersonInfo> otherPersonInfoList = new ArrayList<>();
+
+
+        for (FireResponse fireResponse : fireResponseMap.values()) {
+            List<Resident> residentList = fireResponse.getResidents();
+
+            if (fireResponse.getFireStationNumber().equalsIgnoreCase(stationNumber)) {
+
+                if (fireStationPersonnel.getStationNumber() == null) {
+                    fireStationPersonnel.setStationNumber(stationNumber);
+                }
+
+                if (fireStationPersonnel.getOtherPersonInfoList() == null) {
+                    fireStationPersonnel.setOtherPersonInfoList(new ArrayList<>());
+                }
+
+                for (Resident resident : residentList) {
+                    String[] splitName = resident.getName().split(" ");
+
+                    OtherPersonInfo otherPersonInfo = OtherPersonInfo.builder()
+                            .firstName(splitName[0])
+                            .lastName(splitName[1])
+                            .age(resident.getAge())
+                            .address(fireResponse.getAddress())
+                            .build();
+
+                    otherPersonInfoList.add(otherPersonInfo);
+                }
+            }
+        }
         return null;
     }
 
+    //TODO:: working to create a helper method.
+//    private OtherPersonInfo
 }
