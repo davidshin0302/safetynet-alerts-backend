ab369c1b4b59a5e0ae4a3455a85f147114b3de36
davidshin0302 <david.shin0302@gmail.com>
2024-12-14 01:07:59 -0500

    sprint3: added @Builder lombok.

--- src/main/java/com/safetynet/alerts/view/FireStationPersonnel.java
+++ src/main/java/com/safetynet/alerts/view/FireStationPersonnel.java
@@ -1,14 +1,12 @@
 package com.safetynet.alerts.view;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class FireStationPersonnel {
